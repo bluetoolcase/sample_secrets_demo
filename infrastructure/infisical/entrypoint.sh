08c4398f2a668b7e317494dd2b260b302451b3a6
Joseph Galea <joseph.galea@bluetoolcase.com>
2025-07-27 19:18:59 +0200

    using secrets for containers

---
+++ infrastructure/infisical/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Read the AUTH_SECRET from the mounted secret file
+export AUTH_SECRET=$(cat /run/secrets/infisical_auth_secret)
+
+export ROOT_ENCRYPTION_KEY=$(cat /run/secrets/infisical_root_encryption_key)
+export AUTH_SECRET=$(cat /run/secrets/infisical_auth_secret)
+export DB_CONNECTION_URI=$(cat /run/secrets/infisical_db_connection_uri)
+
+# Execute the original Infisical entrypoint command
+# You'll need to find the actual command Infisical uses to start.
+# It's often something like "npm run start" or a direct executable.
+# Check Infisical's Dockerfile or documentation for its default CMD or ENTRYPOINT.
+/backend/./standalone-entrypoint.sh
+exec "$@"
